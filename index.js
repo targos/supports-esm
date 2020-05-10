1c268aac208351a1edc22ce5d3c0086ba1518749
Myles Borins <myles.borins@gmail.com>
2020-05-10 11:52:03 -0400

    fixup: oops

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function supportsESM(baseVersion=process.version, flags=process.execArgv) {
   const hasExperimentalModulesFlag = flags.includes('--experimental-modules');
   // unflagged in 14.x
   return satisfies(baseVersion, '>=14.x') ||
-    // modern implementation with a flag in 13.7 and higher
-    satisfies(baseVersion, '^13.7') && hasExperimentalModulesFlag ||
+    // modern implementation without a flag in 13.7 and higher
+    satisfies(baseVersion, '^13.7') ||
     // modern implementation with a flag in 12.16 and higher
     satisfies(baseVersion, '^12.16') && hasExperimentalModulesFlag;
 }
--- test/lib.js
+++ test/lib.js
@@ -13,23 +13,29 @@ tap.test('no argument', t => {
 });
 
 tap.test('manual version overide', t => {
+  t.ok(supportsESM('13.7.0'));
+  t.ok(supportsESM('13.10.0'));
   t.ok(supportsESM('14.0.0'));
+  t.ok(supportsESM('14.2.0'));
   t.notOk(supportsESM('8.0.0'));
   t.notOk(supportsESM('9.0.0'));
   t.notOk(supportsESM('10.0.0'));
+  t.notOk(supportsESM('12.16.0'));
   t.notOk(supportsESM('12.0.0'));
   t.notOk(supportsESM('13.0.0'));
   t.end();
 });
 
 tap.test('manual version overide and flag overide', t => {
-  t.ok(supportsESM('14.0.0', '--experimental-modules'));
   t.ok(supportsESM('12.16.0', '--experimental-modules'));
   t.ok(supportsESM('13.7.0', '--experimental-modules'));
+  t.ok(supportsESM('14.2.0', '--experimental-modules'));
   t.notOk(supportsESM('8.0.0', '--experimental-modules'));
   t.notOk(supportsESM('9.0.0', '--experimental-modules'));
   t.notOk(supportsESM('10.0.0', '--experimental-modules'));
   t.notOk(supportsESM('12.0.0', '--experimental-modules'));
+  t.notOk(supportsESM('12.15.0', '--experimental-modules'));
   t.notOk(supportsESM('13.0.0', '--experimental-modules'));
+  t.notOk(supportsESM('13.6.0', '--experimental-modules'));
   t.end();
 });

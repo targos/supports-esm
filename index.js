1d85e020d391b6d72d7ea7f06083af5a4fe70fe9
Myles Borins <myles.borins@gmail.com>
2020-05-10 11:32:35 -0400

    initial implementation

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const {satisfies} = require('semver');
+
+function supportsESM(baseVersion=process.version, flags=process.execArgv) {
+  const hasExperimentalModulesFlag = flags.includes('--experimental-modules');
+  // unflagged in 14.x
+  return satisfies(baseVersion, '>=14.x') ||
+    // modern implementation with a flag in 13.7 and higher
+    satisfies(baseVersion, '^13.7') && hasExperimentalModulesFlag ||
+    // modern implementation with a flag in 12.16 and higher
+    satisfies(baseVersion, '^12.16') && hasExperimentalModulesFlag;
+}
+
+module.exports = supportsESM;

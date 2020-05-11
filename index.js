d890a7139cbb8fe89f2bd472aefd201ef87412c6
Michaël Zasso <targos@protonmail.com>
2020-05-11 08:17:51 +0200

    fix: support prerelease versions of Node.js (#2)

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 
 const {satisfies} = require('semver');
 
+const options = {includePrerelease: true};
+
 function supportsESM(baseVersion=process.version, flags=process.execArgv) {
   const hasExperimentalModulesFlag = flags.includes('--experimental-modules');
   // unflagged in 14.x
-  return satisfies(baseVersion, '>=14.x') ||
+  return satisfies(baseVersion, '>=14.x', options) ||
     // modern implementation without a flag in 13.7 and higher
-    satisfies(baseVersion, '^13.7') ||
+    satisfies(baseVersion, '^13.7', options) ||
     // modern implementation with a flag in 12.16 and higher
-    satisfies(baseVersion, '^12.16') && hasExperimentalModulesFlag;
+    satisfies(baseVersion, '^12.16', options) && hasExperimentalModulesFlag;
 }
 
 module.exports = supportsESM;
--- test/lib.js
+++ test/lib.js
@@ -17,17 +17,20 @@ tap.test('manual version overide', t => {
   t.ok(supportsESM('13.10.0'));
   t.ok(supportsESM('14.0.0'));
   t.ok(supportsESM('14.2.0'));
+  t.ok(supportsESM('14.3.0-rc.1'));
   t.notOk(supportsESM('8.0.0'));
   t.notOk(supportsESM('9.0.0'));
   t.notOk(supportsESM('10.0.0'));
   t.notOk(supportsESM('12.16.0'));
   t.notOk(supportsESM('12.0.0'));
   t.notOk(supportsESM('13.0.0'));
+  t.notOk(supportsESM('13.0.0-rc.1'));
   t.end();
 });
 
 tap.test('manual version overide and flag overide', t => {
   t.ok(supportsESM('12.16.0', '--experimental-modules'));
+  t.ok(supportsESM('12.17.0-rc.1', '--experimental-modules'));
   t.ok(supportsESM('13.7.0', '--experimental-modules'));
   t.ok(supportsESM('14.2.0', '--experimental-modules'));
   t.notOk(supportsESM('8.0.0', '--experimental-modules'));
